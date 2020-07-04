a5c44c6d1e5423d5b37f2aca28ea40b61f906526
unknown <rafaelbatista.sth@gmail.com>
2020-07-04 15:48:54 -0300

    configuration aqlite

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const db = require("./db/connection");
 
 const PORT = 3000;
 
@@ -7,6 +8,16 @@ app.listen(PORT, () => {
   console.log(`O Express está rodenado na porta ${PORT}`);
 });
 
+//db Connection
+db.authenticate()
+  .then(() => {
+    console.log("Conectado com o DB");
+  })
+  .catch((err) => {
+    console.log("Erro na connection com o DB", err);
+  });
+
+//Routes
 app.get("/", (req, res) => {
   res.send("Está Online");
 });
