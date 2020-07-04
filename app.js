15cf87f531bf3ce42f418c67acaab02211cbdb8b
unknown <rafaelbatista.sth@gmail.com>
2020-07-04 17:04:25 -0300

    create route

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const db = require("./db/connection");
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`O Express está rodenado na porta ${PORT}`);
 });
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //db Connection
 db.authenticate()
@@ -21,3 +25,6 @@ db.authenticate()
 app.get("/", (req, res) => {
   res.send("Está Online");
 });
+
+//jobs routes
+app.use("/jobs", require("./routes/jobs"));
